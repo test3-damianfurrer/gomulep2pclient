8bda601ad77305e0d45467eab676e47a387d1b82
test3-damianfurrer <110646054+test3-damianfurrer@users.noreply.github.com>
2023-01-09 14:12:22 +0100

    use gomule-SockSrvClient

--- emule/messages.go
+++ emule/messages.go
@@ -2,7 +2,7 @@ package emule
 
 import (
 	"fmt"
-	util "github.com/AltTechTools/gomule-tst/emule@SockSrvClient"
+	util "github.com/AltTechTools/gomule-SockSrvClient/emule"
 	"net"
 	libdeflate "github.com/4kills/go-libdeflate/v2" //libdeflate.Compressor
 )
